348d2cace5bd07d2219406d9fc20b4c5ae3e6cad
aleksandar <jocovic099@gmail.com>
2020-01-30 19:12:25 +0100

    proizvodiNew

--- js/productSlider.js
+++ js/productSlider.js
@@ -2,7 +2,7 @@ $('.owl-carousel').owlCarousel({
   loop: true,
   margin: 10,
   nav: true,
-  items: 4,
+  items: 6,
   autoplay: true,
   autoplayTimeout: 8000,
   navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"],
@@ -24,7 +24,7 @@ $('.owl-carousel').owlCarousel({
     }
   }
 });
-
+// nzm dal treba
 $(document).on('scroll', function () {
   if ($(document).scrollTop() > 200) {
     $('header').addClass('navResize');
@@ -39,6 +39,7 @@ function myFunction(imgs) {
   var expandImg = document.getElementById("expandedImg");
   var imgText = document.getElementById("imgtext");
   expandImg.src = imgs.src;
+
   imgText.innerHTML = imgs.alt;
   expandImg.parentElement.style.display = "block";
 }
